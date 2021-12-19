2679d5f3f02dd4ca16e158840104687b73fd5b85
LebedevND <qw.lebedev@yandex.ru>
2021-12-19 19:57:10 +0300

    HW11 DONE

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"io"
+	"net"
 	"time"
 )
 
@@ -12,10 +14,50 @@ type TelnetClient interface {
 	Receive() error
 }
 
-func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+type Client struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
+}
+
+func (c *Client) Close() error {
+	err := c.conn.Close()
+	return err
+}
+
+func (c *Client) Connect() error {
+	conn, err := net.DialTimeout("tcp", c.address, c.timeout)
+	if err != nil {
+		return err
+	}
+	c.conn = conn
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (c *Client) Send() error {
+	/* почему не проходит тест test.sh с таким вариантом? при ручном тестировании работает корректно
+	buf, _ := io.ReadAll(c.in)
+	_, err := c.conn.Write(buf)
+	*/
+
+	reader := bufio.NewReader(c.in)
+	_, err := reader.WriteTo(c.conn)
+	return err
+}
+
+func (c *Client) Receive() error {
+	buf, _ := io.ReadAll(c.conn)
+	_, err := c.out.Write(buf)
+	return err
+}
+
+func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
+	return &Client{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
